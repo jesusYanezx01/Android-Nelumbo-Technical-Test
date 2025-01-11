60212cb6f538ee4cec60bcfd8256bfb9b75ee2f9
Jesus Arturo Yañez Patiño <jesusyanezx01@gmail.com>
2025-01-10 23:52:19 -0500

    Home screen design completion

--- app/src/main/java/com/example/nelumbotechnicaltest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/nelumbotechnicaltest/ui/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.nelumbotechnicaltest.ui.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,16 +18,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
+import androidx.compose.material.icons.automirrored.outlined.ArrowForwardIos
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Store
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -34,15 +42,30 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.text.color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.nelumbotechnicaltest.data.models.Request
 import com.example.nelumbotechnicaltest.domain.AppTimeUtil
 import com.example.nelumbotechnicaltest.ui.navigation.Screens
+import kotlin.text.append
 
 @Composable
 fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hiltViewModel()) {
@@ -62,24 +85,72 @@ fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hilt
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                        .clickable { }
+                        .height(48.dp)
+                        .background(color = MaterialTheme.colorScheme.primary),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(
+                        modifier = Modifier.padding(start = 8.dp),
+                        imageVector = Icons.Default.Store,
+                        contentDescription = "Tiendas",
+                        tint = Color.White
+                    )
+                    Text(
+                        text = "Todas las tiendas",
+                        modifier = Modifier.padding(4.dp),
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Icon(
+                        modifier = Modifier.padding(end = 8.dp),
+                        imageVector = Icons.AutoMirrored.Outlined.ArrowForwardIos,
+                        contentDescription = "Todas las tiendas",
+                        tint = Color.White
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                        .drawBehind {
+                            val strokeWidth = 1.dp.toPx()
+                            val y =
+                                size.height - strokeWidth / 2
+                            drawLine(
+                                color = Color.LightGray,
+                                start = Offset(
+                                    0f,
+                                    y
+                                ),
+                                end = Offset(
+                                    size.width,
+                                    y
+                                ),
+                                strokeWidth = strokeWidth
+                            )
+                        },
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     TextButton(
                         onClick = { },
                         modifier = Modifier
-                            .padding(4.dp)
-                            .offset(y = (-16).dp)
+                            .padding(start = 8.dp, end = 4.dp)
+
                     ) {
                         Text(
                             text = "Filtrar por",
-                            modifier = Modifier.padding(4.dp)
+                            modifier = Modifier.padding(4.dp),
+                            color = Color.Black
                         )
                         Spacer(modifier = Modifier.width(4.dp))
                         Icon(
                             imageVector = Icons.Default.FilterList,
                             contentDescription = "Filtrar",
+                            tint = Color.Black,
                             modifier = Modifier.size(16.dp)
                         )
                     }
@@ -87,8 +158,10 @@ fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hilt
                     FloatingActionButton(
                         onClick = {},
                         modifier = Modifier
-                            .size(56.dp)
-                            .offset(y = 24.dp),
+                            .size(72.dp)
+                            .offset(y = 32.dp),
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape
                     ) {
                         Icon(
                             imageVector = Icons.Default.Add,
@@ -100,18 +173,19 @@ fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hilt
                     TextButton(
                         onClick = { },
                         modifier = Modifier
-                            .padding(4.dp)
-                            .offset(y = (-16).dp)
+                            .padding(start = 4.dp, end = 8.dp)
                     ) {
                         Text(
                             text = "Ordenar por",
-                            modifier = Modifier.padding(4.dp)
+                            modifier = Modifier.padding(4.dp),
+                            color = Color.Black
                         )
                         Spacer(modifier = Modifier.width(4.dp))
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.Sort,
                             contentDescription = "Ordenar",
-                            modifier = Modifier.size(16.dp)
+                            modifier = Modifier.size(16.dp),
+                            tint = Color.Black
                         )
                     }
                 }
@@ -154,10 +228,20 @@ fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hilt
 
 @Composable
 fun RequestItem(request: Request, navController: NavController) {
+    val grayColor = Color(0xFF9d9d9d)
+    val boldStyle = SpanStyle(fontWeight = FontWeight.Bold)
+    val normalStyle = SpanStyle(color = grayColor)
+
     Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
+            .padding(horizontal = 32.dp, vertical = 16.dp)
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(4.dp)
+            ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        )
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row {
@@ -165,23 +249,29 @@ fun RequestItem(request: Request, navController: NavController) {
                     modifier = Modifier
                         .padding(horizontal = 8.dp, vertical = 4.dp)
                 ) {
-                    Text(text = "# ${request.id}")
+                    Text(text = "# ${request.id}", color = Color(0xFF9d9d9d))
                 }
                 Spacer(modifier = Modifier.weight(1f))
                 Box(
                     modifier = Modifier
-                        .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+                        .background(
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = RoundedCornerShape(8.dp)
+                        )
                         .padding(horizontal = 8.dp, vertical = 4.dp)
                 ) {
                     Text(
                         text = request.type ?: "N/A",
-                        color = Color.Black
+                        color = Color.White
                     )
                 }
             }
             Spacer(modifier = Modifier.height(4.dp))
             Text(
-                text = request.name ?: "N/A",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append(request.name ?: "N/A") }
+                },
+                fontSize = 24.sp,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
@@ -189,53 +279,102 @@ fun RequestItem(request: Request, navController: NavController) {
             Row {
                 Box(
                     modifier = Modifier
-                        .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .height(30.dp)
+                        .background(
+                            color = Color(0xFFec6666),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(4.dp)
                 ) {
                     Text(
                         text = request.priority?.name ?: "N/A",
-                        color = Color.Black
+                        color = Color.White,
+                        fontSize = 12.sp
                     )
                 }
-                Spacer(modifier = Modifier.width(16.dp))
+                Spacer(modifier = Modifier.width(4.dp))
                 Box(
                     modifier = Modifier
-                        .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .height(30.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .border(
+                            border = BorderStroke(
+                                1.dp,
+                                MaterialTheme.colorScheme.primary
+                            ),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(4.dp)
                 ) {
                     Text(
                         text = request.status?.description ?: "N/A",
-                        color = Color.Black
+                        color = MaterialTheme.colorScheme.primary,
+                        fontSize = 12.sp
                     )
                 }
             }
+            Spacer(modifier = Modifier.width(4.dp))
             Text(
-                text = "Solicitada el: ${AppTimeUtil.formatIsoDateToCustom(request.createTime)} ",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Solicitada el: ") }
+                    withStyle(normalStyle) { append(AppTimeUtil.formatIsoDateToCustom(request.createTime)) }
+                },
+                fontSize = 16.sp,
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Área: ${request.area?.name}",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Área: ") }
+                    withStyle(normalStyle) { append(request.area?.name ?: "N/A") }
+                },
+                fontSize = 16.sp
             )
             Text(
-                text = "Departamento: ${request.department?.name}",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Departamento: ") }
+                    withStyle(normalStyle) { append(request.department?.name ?: "N/A") }
+                },
+                fontSize = 16.sp
             )
             Text(
-                text = "Unidad: ${request.store?.name}",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Unidad: ") }
+                    withStyle(normalStyle) { append(request.store?.name ?: "N/A") }
+                },
+                fontSize = 16.sp
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Creador: ${request.createdByUser?.firstName} ${request.createdByUser?.lastName}",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Creador: ") }
+                    withStyle(normalStyle) { append("${request.createdByUser?.firstName ?: "N/A"} ${request.createdByUser?.lastName ?: "N/A"}") }
+                },
+                fontSize = 16.sp
             )
             Text(
-                text = "Proveedor: ${request.department?.userManage?.firstName ?: "N/A"} ${request.department?.userManage?.lastName ?: "N/A"}"
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Proveedor: ") }
+                    withStyle(normalStyle) { append("${request.department?.userManage?.firstName ?: "N/A"} ${request.department?.userManage?.lastName ?: "N/A"}") }
+                },
+                fontSize = 16.sp
             )
             Text(
-                text = "Solucionador: ${request.userAttendingId ?: "N/A"}",
+                text = buildAnnotatedString {
+                    withStyle(boldStyle) { append("Solucionador: ") }
+                    withStyle(normalStyle) { append(request.userAttendingId?.toString() ?: "N/A") }
+                },
+                fontSize = 16.sp
             )
             Spacer(modifier = Modifier.height(8.dp))
             Button(
                 onClick = { navController.navigate("${Screens.REQUEST_DETAIL.title}/${request.id}") },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                shape = RoundedCornerShape(8.dp)
             ) {
                 Text(text = "Ver detalle")
             }
--- app/src/main/java/com/example/nelumbotechnicaltest/ui/home/TopBar.kt
+++ app/src/main/java/com/example/nelumbotechnicaltest/ui/home/TopBar.kt
@@ -2,26 +2,31 @@ package com.example.nelumbotechnicaltest.ui.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Store
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.nelumbotechnicaltest.R
 import com.example.nelumbotechnicaltest.ui.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,23 +37,23 @@ fun CustomTopAppBar(navController: NavHostController) {
     val currentRoute = navBackStackEntry?.destination?.route ?: ""
 
     TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFF313131)
+        ),
         title = {
             if (currentRoute.startsWith(Screens.HOME.title)){
                 Text(
-                    text = "Todas las tiendas",
+                    text = stringResource(R.string.app_name),
                     style = MaterialTheme.typography.titleMedium,
+                    color = Color.White
                 )
             }
         },
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .statusBarsPadding(),
         navigationIcon = {
-            if (currentRoute.startsWith(Screens.HOME.title)){
-                Icon(
-                    imageVector = Icons.Default.Store,
-                    contentDescription = "Tiendas"
-                )
-            } else if (currentRoute.startsWith(Screens.REQUEST_DETAIL.title)){
+             if (currentRoute.startsWith(Screens.REQUEST_DETAIL.title)){
                 Row {
                     Box(
                         modifier = Modifier
@@ -57,26 +62,51 @@ fun CustomTopAppBar(navController: NavHostController) {
                         IconButton(onClick = {
                             navController.navigateUp()
                         }) {
-                            Icon(imageVector = Icons.Outlined.ArrowBackIosNew, contentDescription = null)
+                            Icon(
+                                imageVector = Icons.Outlined.ArrowBackIosNew,
+                                contentDescription = null,
+                                tint = Color.White
+                            )
                         }
-                    }
-
-                    Spacer(modifier = Modifier.weight(1f))
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 4.dp)
-                    ) {
-                        Icon(imageVector = Icons.Default.Notifications, contentDescription = null)
                     }
                 }
             }
         },
         actions = {
-            if (currentRoute.startsWith(Screens.HOME.title)){
+            if (currentRoute.startsWith(Screens.HOME.title)) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
-                    contentDescription = "Navegar a todas las tiendas"
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Mi perfil",
+                    tint = Color.White
                 )
+                Icon(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = "Notificaciones",
+                    tint = Color.White
+                )
+            } else if (currentRoute.startsWith(Screens.REQUEST_DETAIL.title)) {
+                BadgedBox(
+                    badge = {
+                        Badge {
+                            Text(
+                                text = "3+",
+                                color = Color.White
+                            )
+                        }
+                    }
+                ) {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Notifications,
+                            contentDescription = "Notificaciones",
+                            tint = Color.White
+                        )
+                    }
+                }
             }
         }
     )
